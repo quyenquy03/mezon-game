5e742196ca1960609271ff164fde998c070911b8
Nguyen Ta Quyen <ta2k3quyen@gmail.com>
2024-11-25 17:12:19 +0700

    design room page

--- src/client/assets/javascript/client.js
+++ src/client/assets/javascript/client.js
@@ -1,3 +1,138 @@
 console.log("clientjs loader");
 
-var socket = io();
+const socket = io("http://localhost:3000"); // Kết nối đến server
+
+// Gửi thông tin người dùng lên server
+
+// Xử lý ngắt kết nối (bên client)
+socket.on("disconnect", () => {
+  console.log("Disconnected from server");
+});
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const generateUser = () => {
+  const userId = randomNumber(1000, 9999);
+  return {
+    userId: userId,
+    name: "User " + userId,
+    avatar: "https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611765.jpg",
+  };
+};
+const user = generateUser();
+const fetchUser = () => {
+  setTimeout(() => {
+    socket.emit("userInfo", user);
+  }, 1000);
+};
+fetchUser();
+
+const createNewRoom = () => {
+  const roomName = document.getElementById("room-name")?.value;
+  const roomMaxUser = document.getElementById("room-max-user")?.value;
+  const roomPassword = document.getElementById("room-password")?.value;
+  const roomUsePassword = document.getElementById("room-use-password").checked;
+  const roomBet = document.getElementById("room-bet")?.value;
+
+  socket.emit("createRoom", {
+    roomId: randomNumber(100000, 999999),
+    roomName,
+    roomMaxUser,
+    roomPassword,
+    roomUsePassword,
+    roomBet,
+    owner: user.userId,
+  });
+};
+
+socket.on("roomCreated", (roomInfo) => {
+  console.log("Room created:", roomInfo);
+});
+
+const renderListRoom = (listRooms) => {
+  const listRoomElement = document.querySelector(".room-list");
+  listRoomElement.innerHTML = "";
+  listRooms.forEach((room) => {
+    const roomElement = document.createElement("div");
+    roomElement.classList.add("col", "col-xs-12", "col-sm-6", "col-xl-4");
+    roomElement.innerHTML = `
+      <div class="room-item">
+              <div class="room-name">
+                <span class="room-name-text">${room.roomId}</span>
+              </div>
+              <div class="room-join">
+                <button class="room-join-btn">JOIN</button>
+              </div>
+              <div class="room-bet">
+                <img class="room-bet-bg" src="./assets/images/bg-cuoc.png" alt="" />
+                <div class="room-bet-info">
+                  <span class="room-bet-text">${room.roomBet}</span>
+                  <img class="room-bet-coin" src="./assets/images/coin.png" alt="" />
+                </div>
+              </div>
+              <div class="room-owner">
+                <img class="room-owner-icon" src="./assets/images/user.png" alt="" />
+                <span class="room-owner-name">username</span>
+              </div>
+            </div>
+    `;
+    listRoomElement.appendChild(roomElement);
+  });
+};
+socket.emit("listRooms");
+socket.on("listRooms", (rooms) => {
+  renderListRoom(rooms);
+  console.log("List rooms:", rooms);
+});
+
+const renderListUser = (listUsers) => {
+  // Chọn đúng phần tử DOM có class "user-list"
+  const listUserElement = document.querySelector(".user-list");
+
+  // Xóa nội dung hiện có trong phần tử
+  listUserElement.innerHTML = "";
+
+  // Render danh sách người dùng
+  listUsers.forEach((user) => {
+    // Tạo phần tử "user-item"
+    const userElement = document.createElement("div");
+    userElement.classList.add("user-item");
+    userElement.innerHTML = `
+      <div class="user-avatar">
+        <img src="${user.avatar}" alt="${user.name}" />
+      </div>
+      <div class="user-info">
+        <span class="user-name">${user.name}</span>
+      </div>
+    `;
+
+    // Thêm phần tử vào danh sách
+    listUserElement.appendChild(userElement);
+  });
+};
+socket.emit("listUsers");
+socket.on("listUsers", (users) => {
+  renderListUser(users);
+  console.log("List users:", users);
+});
+
+const renderUserInfo = (userInfo) => {
+  const userInfoElement = document.querySelector(".account-box");
+
+  if (!userInfoElement) {
+    console.error("Element with class 'account-box' not found.");
+    return;
+  }
+
+  userInfoElement.innerHTML = `
+    <div class="avatar">
+      <img src="${userInfo.avatar}" alt="${userInfo.name}" />
+    </div>
+    <div class="name">
+      <span class="user-name">${userInfo.name}</span>
+    </div>
+  `;
+};
+renderUserInfo(user);
--- src/index.js
+++ src/index.js
@@ -20,23 +20,109 @@ const server = app.listen(3000, function () {
   console.log("Example app listening on port 3000 with domain http://localhost:3000");
 });
 
-const io = new SocketServer(server, {
-  cors: {
-    // origin: [ENV.ORIGIN],
-    methods: ["GET", "POST"],
-    credentials: true,
+const connectedUsers = [
+  {
+    id: "1234",
+    userId: 1234,
+    name: "User 1234",
+    avatar: "https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611765.jpg",
+    socketId: "socket-1234",
   },
-});
+  {
+    id: "5678",
+    userId: 5678,
+    name: "User 5678",
+    avatar: "https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611765.jpg",
+    socketId: "socket-5678",
+  },
+];
+const listRooms = [
+  {
+    roomId: "123456",
+    roomName: "Phòng 1",
+    roomMaxUser: 8,
+    roomPassword: null,
+    roomUsePassword: false,
+    roomBet: 1000,
+    owner: 1234, // ID của người chủ phòng
+  },
+  {
+    roomId: "234567",
+    roomName: "Phòng 1",
+    roomMaxUser: 8,
+    roomPassword: null,
+    roomUsePassword: false,
+    roomBet: 1000,
+    owner: 1234, // ID của người chủ phòng
+  },
+  {
+    roomId: "234567",
+    roomName: "Phòng 1",
+    roomMaxUser: 8,
+    roomPassword: null,
+    roomUsePassword: false,
+    roomBet: 1000,
+    owner: 1234, // ID của người chủ phòng
+  },
+  {
+    roomId: "234567",
+    roomName: "Phòng 1",
+    roomMaxUser: 8,
+    roomPassword: null,
+    roomUsePassword: false,
+    roomBet: 1000,
+    owner: 1234, // ID của người chủ phòng
+  },
+];
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+const disconnectUser = (socket) => {
+  const index = connectedUsers.findIndex((user) => user.id === socket.id);
+  const user = connectedUsers[index];
+  if (index !== -1) {
+    connectedUsers.splice(index, 1); // Xóa khỏi mảng
+  }
+};
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+const createdRoom = (roomInfo) => {
+  listRooms.push(roomInfo);
+};
+const setupSocketServer = (server) => {
+  const io = new SocketServer(server, {
+    cors: {
+      // origin: [ENV.ORIGIN],
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
   });
 
-  socket.on("createGame", (data) => {
-    console.log("createGame", data);
-    io.emit("createGame", data);
+  io.on("connection", (socket) => {
+    // Nhận thông tin từ client
+    socket.on("userInfo", (userInfo) => {
+      connectedUsers.push({ id: socket.id, ...userInfo });
+      io.emit("userInfo", userInfo);
+      io.emit("listUsers", connectedUsers);
+    });
+
+    socket.on("listUsers", () => {
+      io.emit("listUsers", connectedUsers);
+    });
+
+    // Tạo phòng mới
+    socket.on("createRoom", (roomInfo) => {
+      createdRoom(roomInfo);
+      io.emit("roomCreated", roomInfo);
+      io.emit("listRooms", listRooms);
+    });
+
+    socket.on("listRooms", () => {
+      io.emit("listRooms", listRooms);
+    });
+
+    // Khi client ngắt kết nối
+    socket.on("disconnect", () => {
+      disconnectUser(socket);
+    });
   });
-});
+};
+
+setupSocketServer(server);
